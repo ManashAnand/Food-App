f6da5b8512c3115f81c8321aab95c198e8266d34
MANASH <anandmanash321@gmail.com>
2022-10-05 23:37:41 +0530

    initial food app

---
+++ src/components/Card.jsx
@@ -0,0 +1,56 @@
+import React,{useEffect,useState} from 'react'
+
+export default function Card() {
+    const [search,setSearch] = useState("");
+    const [btnValue,setbtnValue] = useState("orange");
+    const apikey = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${btnValue}?query=${btnValue}&apiKey=96a3052c31c549b697f30aee47939b02`;
+
+    const updateText = (e) => {
+        setSearch(e.target.value);
+    }
+   
+    const [mainData,setMainData] = useState([])
+    useEffect(()=>{
+        getData();
+    },[btnValue])
+    
+    const handleSearch = () => 
+    {
+        setbtnValue(search);
+        setSearch("");
+    }
+    
+    
+    const getData = async () =>
+    {
+        const data = await fetch(apikey);
+        const JSONdata = await data.json();
+        console.log(JSONdata);
+        setMainData(JSONdata);
+    }
+    
+  return (
+    <>
+     <div className="head">Get your Fav Food item here.</div> 
+     <div className="search-container">
+     <div className='search-box'>
+    <input type="text" placeholder='Search...' name="text" className='search-text' autoComplete='off' value={search} onChange={updateText} />
+    <div className="btn" onClick={handleSearch}>Search</div>
+      </div>
+    </div>
+        {
+            mainData.map((maindata)=>{
+                return(
+                    <div className="mainContainer" >
+                        <img src={maindata.image} alt="food image" />
+                        <div className="heading">
+                            <p>{maindata.title}</p>
+                        </div>
+                    </div>
+                )
+            })
+        }
+      
+    </>
+  )
+}
